989c665475088b554435a2247b0d30502e61d9de
DanilaKuprenkovs <125027313+DanilaKuprenkovs@users.noreply.github.com>
2023-03-19 23:21:38 +0000

    Danila Kuprenkovs 221RDB417

--- main.py
+++ main.py
@@ -2,30 +2,42 @@
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    # list to store the finishing times of each thread
+    finish_time = [0] * n
+    # iterate through each job
+    for i in range(m):
+        # find the earliest finishing time among the threads
+        min_finish_time = min(finish_time)
+        # find the thread with the earliest finishing time
+        thread_index = finish_time.index(min_finish_time)
+        # calculate the starting time for the job
+        start_time = finish_time[thread_index]
+        # calculate the finishing time for the job
+        end_time = start_time + data[i]
+        # update the finishing time for the thread
+        finish_time[thread_index] = end_time
+        # append the output pair
+        output.append((thread_index, start_time))
     return output
 
 def main():
-    # TODO: create input from keyboard
+    # read input from keyboard
     # input consists of two lines
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n, m = map(int, input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data = list(map(int, input().split()))
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
+    # calculate the output pairs
+    result = parallel_processing(n, m, data)
     
-    # TODO: print out the results, each pair in it's own line
-
-
-
+    # print out the results, each pair in its own line
+    for pair in result:
+        print(pair[0], pair[1])
+        
 if __name__ == "__main__":
     main()
